1a0ab963c3e810d1a48a77170bca4cbb2e22d5fc
JustRiley <justriley706@gmail.com>
2017-07-19 19:10:40 -0400

    Added user service

--- public/assignment/user/controllers/login.controller.client.js
+++ public/assignment/user/controllers/login.controller.client.js
@@ -4,24 +4,14 @@
         .module("WamApp")
         .controller("loginController", loginController);
 
-//JSON JavaScript object Notation
-    var users = [
-        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-    ];
-
-    function loginController($scope, $location) {
+    function loginController($scope, $location, userService) {
         $scope.login = function (user) {
-            for (var u in users) {
-                //can't just do var user= u; since u is just an index
-                var _user = users[u];
-                if (_user.username === user.username && _user.password === user.password) {
-                    $location.url("profile/" + _user._id);
-                }
+            var user = userService.findUserByUsernameAndPassword(user.username, user.password);
+            if (user === null){
+                $scope.errorMessage = "User not found";
+            } else {
+                $location.url("profile/" + user._id);
             }
-            $scope.errorMessage = "User not found";
         };
     }
 })();
