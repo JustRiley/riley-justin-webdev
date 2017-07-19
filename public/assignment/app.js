954c4719bd571cb6d823439f0a7069d3f8e4d8f6
JustRiley <justriley706@gmail.com>
2017-07-19 17:36:46 -0400

    Converting to dynamic content

---
+++ public/assignment/app.js
@@ -0,0 +1,41 @@
+var app = angular.module("WamApp", ["ngRoute"]);
+
+app.controller("loginController", loginController);
+
+app.config(configuration);
+
+function configuration($routeProvider) {
+    $routeProvider
+        .when("/login", {
+            templateUrl: "login.html"
+        })
+        .when("/register", {
+            templateUrl: "register.html"
+    })
+        .when("/profile", {
+            templateUrl: "profile.html"
+        })
+}
+
+function loginController($scope, $location) {
+
+//JSON JavaScript object Notation
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+
+    $scope.login = function (user) {
+        for(var u in users) {
+            //can't just do var user= u; since u is just an index
+            var _user = users[u];
+            if (_user.username === user.username && _user.password === user.password) {
+                $location.url("profile");
+            }
+        }
+        $scope.errorMessage = "User not found";
+    };
+}
