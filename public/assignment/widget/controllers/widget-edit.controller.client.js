8c1daa223d100e0353a56655b3174a73679db52f
JustRiley <justriley706@gmail.com>
2017-07-22 15:16:02 -0400

    Widget switch

--- public/assignment/widget/controllers/widget-edit.controller.client.js
+++ public/assignment/widget/controllers/widget-edit.controller.client.js
@@ -9,9 +9,23 @@
         model.websiteId = $routeParams.websiteId;
         model.pageId = $routeParams.pageId;
         model.wgid = $routeParams.wgid;
+        model.updateWidget = updateWidget;
+        model.deleteWidget = deleteWidget;
+        //TODO: editWidget, Back button, profile button
 
         function init() {
+            model.widget = widgetService.findWidgetById(model.wgid);
         }
         init();
+
+        function updateWidget() {
+            widgetService.updateWidget(model.wgid, model.widget);
+            $location.url("/user/"+ model.userId +"/website/" + model.websiteId + "/page/" + model.pageId + "/widget/");
+        }
+
+        function deleteWidget() {
+            widgetService.deleteWidget(model.wgid);
+            $location.url("/user/"+ model.userId +"/website/" + model.websiteId + "/page/" + model.pageId + "/widget/");
+        }
     }
 })();
