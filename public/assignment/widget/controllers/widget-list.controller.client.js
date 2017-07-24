75e699fae33c47dd912c8d9c53bf69583bf2fccb
JustRiley <justriley706@gmail.com>
2017-07-24 19:21:10 -0400

    Youtube bug fix

--- public/assignment/widget/controllers/widget-list.controller.client.js
+++ public/assignment/widget/controllers/widget-list.controller.client.js
@@ -5,17 +5,31 @@
     angular.module("WamApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController($location, $routeParams, widgetService) {
+    function widgetListController($sce, $routeParams, widgetService) {
         var model = this;
 
         model.userId = $routeParams.userId;
         model.websiteId = $routeParams.websiteId;
         model.pageId = $routeParams.pageId;
+        model.trustHtmlContent = trustHtmlContent;
+        model.trustUrlResource = trustUrlResource;
 
         function init() {
             model.widgets = widgetService.findWidgetsByPageId(model.pageId);
         }
         init();
+//TODO:Youtube bug fix
+        function trustUrlResource(url) {
+            console.log(url);
+            var youtubeUrl = "https://www.youtube.com/embed/";
+            var urlParts = url.split("/");
+            youtubeUrl += urlParts[urlParts.length-1];
+            return $sce.trustAsResourceUrl(youtubeUrl);
+        }
+
+        function trustHtmlContent(htmlContent){
+            return $sce.trustAsHtml(htmlContent);
+        }
     }
 })();
 //TODO:Widget list Youtube links won't load? ""Can't interpolate: {{widget.url}}
