e0afd70d48f0be8065ce232bbcba337bbd657934
JustRiley <justriley706@gmail.com>
2017-07-25 11:31:41 -0400

    Youtube bug fix

--- public/assignment/widget/controllers/widget-list.controller.client.js
+++ public/assignment/widget/controllers/widget-list.controller.client.js
@@ -5,18 +5,30 @@
     angular.module("WamApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController($location, $routeParams, widgetService) {
+    function widgetListController($sce, $routeParams, widgetService) {
         var model = this;
 
         model.userId = $routeParams.userId;
         model.websiteId = $routeParams.websiteId;
         model.pageId = $routeParams.pageId;
+        model.trustUrlResource = trustUrlResource;
+        model.trustHtmlContent = trustHtmlContent;
 
         function init() {
             model.widgets = widgetService.findWidgetsByPageId(model.pageId);
         }
         init();
+
+        function trustUrlResource(url) {
+            console.log(url);
+            var youtubeUrl = "https://www.youtube.com/embed/";
+            var urlParts = url.split("/");
+            youtubeUrl += urlParts[urlParts.length-1];
+            return $sce.trustAsResourceUrl(youtubeUrl);
+        }
+
+        function trustHtmlContent(htmlContent){
+            return $sce.trustAsHtml(htmlContent);
+        }
     }
 })();
-//TODO:Widget list Youtube links won't load? ""Can't interpolate: {{widget.url}}
-//Error: [$sce:insecurl] Blocked loading resource from url not allowed by $sceDelegate policy.
