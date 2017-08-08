a08585d7b3f3dc8047681484a2ec346ffc8dc750
JustRiley <justriley706@gmail.com>
2017-08-08 16:07:46 -0400

    Setup for widget model refactor

--- assignment/models/page.model.server.js
+++ assignment/models/page.model.server.js
@@ -11,7 +11,30 @@ pageModel.findPageById = findPageById;
 pageModel.updatePage = updatePage;
 pageModel.findPagesForWebsite = findPagesForWebsite;
 pageModel.deletePage = deletePage;
+
+pageModel.addWidget = addWidget;
+pageModel.removeWidget = removeWidget;
+
 module.exports = pageModel;
+
+function addWidget(pageId, widgetId) {
+    return pageModel
+        .findPageById(pageId)
+        .then(function (page) {
+            page.widgets.push(widgetId);
+            return page.save();
+        })
+}
+
+function removeWidget(pageId, widgetId) {
+    return pageModel
+        .findPageById(pageId)
+        .then(function (page) {
+            var index = page.widgets.indexOf(widgetId);
+            page.widgets.splice(index, 1);
+            return page.save();
+        })
+}
 
 function deletePage(websiteId, pageId) {
     return pageModel
