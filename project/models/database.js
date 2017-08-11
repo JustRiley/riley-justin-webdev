83b4f3960373bd9e72b64ebc19d99b407aec6ecc
JustRiley <justriley706@gmail.com>
2017-08-11 16:53:09 -0400

    damn

--- project/models/database.js
+++ project/models/database.js
@@ -5,8 +5,10 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds015750.mlab.com:15750/heroku_bkxx03k1'; // user yours
+    connectionString += '@ds151232.mlab.com:51232/heroku_60r2dvbn'; // user yours
 }
+
+//mongodb://heroku_bkxx03k1:k1k8ndgof4gsm5vge38qfjqq7@ds015750.mlab.com:15750/heroku_bkxx03k1
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
 
